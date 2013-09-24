f583ff3706e8c7c59dd700656df231b146389078
Joshua Kovach <kovach.jc@gmail.com>
2013-09-24 16:40:48 -0400

    Use a default for #create or #new

--- lib/split_datetime/accessors.rb
+++ lib/split_datetime/accessors.rb
@@ -1,30 +1,29 @@
 module SplitDatetime
   module Accessors
     def accepts_split_datetime_for(*attrs)
-      opts = { format: "%F" }
-
-      if attrs.last.class == Hash
-        custom = attrs.delete_at(-1)
-        opts = opts.merge(custom)
-      end
+      opts = { format: "%F" }.merge!(attrs.extract_options!)
 
       attrs.each do |attr|
         attr_accessible "#{attr}_date", "#{attr}_hour", "#{attr}_min", "#{attr}_time" if needs_attr_accessible?
 
+        define_method("#{attr}_or_new") do
+          self.send(attr) || Time.new
+        end
+
         define_method("#{attr}_date=") do |date|
           return unless date.present?
           date = Date.parse(date.to_s)
-          self.send("#{attr}=", self.send(attr).change(year: date.year, month: date.month, day: date.day))
+          self.send("#{attr}=", self.send("#{attr}_or_new").change(year: date.year, month: date.month, day: date.day))
         end
 
         define_method("#{attr}_hour=") do |hour|
           return unless hour.present?
-          self.send("#{attr}=", self.send(attr).change(hour: hour, min: self.send(attr).min))
+          self.send("#{attr}=", self.send("#{attr}_or_new").change(hour: hour))
         end
 
         define_method("#{attr}_min=") do |min|
           return unless min.present?
-          self.send("#{attr}=", self.send(attr).change(min: min))
+          self.send("#{attr}=", self.send("#{attr}_or_new").change(min: min))
         end
         
         composed_of "#{attr}_time".to_sym, class_name: 'DateTime'
@@ -32,7 +31,7 @@ module SplitDatetime
         define_method("#{attr}_time=") do |time|
           return unless time.present?
           time = DateTime.parse(time) unless time.is_a?(Date) || time.is_a?(Time)
-          self.send("#{attr}=", self.send(attr).change(hour: time.hour, min: time.min))
+          self.send("#{attr}=", self.send("#{attr}_or_new").change(hour: time.hour, min: time.min))
         end
 
         define_method("#{attr}_date") do
