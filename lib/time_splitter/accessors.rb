f61e1c57e8291c5526d6132308c39e1b6f5bb69f
Joshua Kovach <kovach.jc@gmail.com>
2013-09-29 02:29:58 -0400

    Add some comments

--- lib/time_splitter/accessors.rb
+++ lib/time_splitter/accessors.rb
@@ -12,9 +12,15 @@ module TimeSplitter
         # Maps the setter for #{attr}_time to accept multipart-parameters for Time
         composed_of "#{attr}_time".to_sym, class_name: 'DateTime' if self.respond_to?(:composed_of)
 
+        # Default instance of the attribute, used if setting an element of the
+        # time attribute before the attribute was sent. Allows us to retrieve a
+        # default value for +#{attr}+ to modify without explicitely overriding
+        # the attr_reader. Defaults to a Time object with all fields set to 0.
         define_method("#{attr}_or_new") do
           self.send(attr) || Time.new(0)
         end
+
+        # Writers
 
         define_method("#{attr}_date=") do |date|
           return unless date.present?
